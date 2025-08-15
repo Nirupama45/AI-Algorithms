dbd3b995e236f75191ccb146fcbe53e709d5d1b0
Nirupama45 <149401579+Nirupama45@users.noreply.github.com>
2025-08-15 21:24:33 +0530

    Add hc.c

---
+++ hc.c
@@ -0,0 +1,82 @@
+#include <stdio.h>
+#include <stdbool.h>
+
+#define n 7
+
+int g[n][n] = {
+    {0,1,1,0,0,0,0},
+    {1,0,1,0,0,1,0},
+    {1,1,0,1,0,0,0},
+    {0,0,1,0,1,0,0},
+    {0,0,0,1,0,0,0},
+    {0,1,0,0,0,0,1},
+    {0,0,0,0,0,1,0}
+};
+
+int h[n];
+
+void compute_heuristic(int goal){
+    int queue[n];
+    int front = 0;
+    int rear = 0;
+    bool visited[n] = {false};
+    
+    for(int i=0; i<n; i++){
+        h[i] = -1;
+    }
+    
+    h[goal] = 0;
+    visited[goal] = true;
+    queue[rear++] = goal;
+    
+    while (front < rear){
+        int i = queue[front++];
+        for(int j =0; j<n; j++){
+            if(g[i][j] && !visited[j]){
+                visited[j] = true;
+                h[j] = h[i] + 1;
+                queue[rear++] = j;
+            }
+        }
+    }
+}
+
+void hc(int start, int goal) {
+    int curr = start;
+
+    while (true) {
+        printf("%d ", curr);
+
+        if (curr == goal) {
+            return;
+        }
+
+        int next = -1;
+        int best_h = h[curr]; 
+
+        for (int i = 0; i < n; i++) {
+            if (g[curr][i] == 1) { 
+                if (h[i] < best_h) { 
+                    best_h = h[i];
+                    next = i;
+                }
+            }
+        }
+
+        if (next == -1) { 
+            printf("Can't move further.\n");
+            return;
+        }
+
+        curr = next; 
+    }
+}
+
+int main() {
+    int start = 0;
+    int goal = 6;
+    compute_heuristic(goal);
+    printf("Path: ");
+    hc(start, goal);
+    return 0;
+}
